52986d7668a5e516c45f195d98212948b6972b1f
Mahdi Choudhury <choudhurymahdi02@gmail.com>
2018-11-10 19:49:58 +0000

    add comments to javascript

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 var slideIndex = 0;
 showSlides();
 function showSlides () {
-        /*console.log("hello, slideIndex")*/
+        //console.log("hello, slideIndex");
     var slides = document.getElementsByClassName("myimages");
      for (var i = 0; i < slides.length; i++) {
-         console.log("Hi");
+         //console.log("Hi");
         slides[i].style.display = "none";
     }
     slideIndex++
